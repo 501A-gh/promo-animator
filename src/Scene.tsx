539d26c7bbcaec15515822051f027b5c4fd7d3ef
501A <art71903design@gmail.com>
2025-03-15 16:28:30 -0700

    Add drei and OrbitControls

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,16 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import { CameraHelper } from "./CameraHelper";
+import { OrbitControls } from "@react-three/drei";
 
-export const Scene = () => {
-  return (
-    <Canvas className="border border-rose-500">
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshPhongMaterial />
-      </mesh>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[0, 0, 5]} color="red" />
-      <CameraHelper />
-    </Canvas>
-  );
-};
+export const Scene = () => (
+  <Canvas
+    className="border border-rose-500"
+    style={{
+      height: "100vh",
+      width: "100vw",
+    }}
+  >
+    <mesh>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshPhongMaterial />
+    </mesh>
+    <ambientLight intensity={0.1} />
+    <directionalLight position={[0, 0, 5]} color="red" />
+    <CameraHelper />
+    <OrbitControls />
+  </Canvas>
+);
