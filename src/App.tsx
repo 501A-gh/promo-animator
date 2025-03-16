b8bc0a26b513e6ee32a06b05b37da1a5822e23a1
501A <art71903design@gmail.com>
2025-03-16 00:52:44 -0700

    Add Radix slider + display keyframes on timeline

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Camera } from "./Camera";
 import { CameraHelper } from "./CameraHelper";
-import { AnimatedBox } from "./AnimatedBox";
+import { Timeline } from "./Timeline";
+// import { AnimatedBox } from "./AnimatedBox";
 
 function App() {
   const data = animationData as AnimationData;
@@ -14,45 +15,58 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(true);
 
   // Handler for slider changes (scrub through the timeline)
-  const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTime(parseFloat(e.target.value));
+  const handleSliderChange = (v: number) => {
+    setTime(v);
     setIsPlaying(false); // Pause when scrubbing manually
   };
 
   return (
     <>
       <div>
-        <div className="flex h-full *:flex-1">
+        <div className="grid grid-cols-2 h-fit border-b border-blue-400">
+          <div className="flex flex-col">
+            <div className="flex-1 flex justify-center items-center">
+              <Canvas
+                className="border border-rose-500 my-auto rounded-md"
+                style={{
+                  aspectRatio: 16 / 9,
+                  height: "fit-content",
+                }}
+              >
+                <color attach="background" args={["#141622"]} />
+                <Camera
+                  data={data}
+                  time={time}
+                  isPlaying={isPlaying}
+                  setTime={setTime}
+                  setIsPlaying={setIsPlaying}
+                />
+                <ambientLight intensity={0.1} />
+                <directionalLight position={[0, 0, 5]} color="red" />
+                <gridHelper args={[20, 20, "#888888", "#444444"]} />
+              </Canvas>
+            </div>
+            <div className="flex justify-between items-center p-4 gap-3 text-black dark:text-white">
+              <button onClick={() => setTime(0)}>Reset</button>
+              <div className="flex items-center gap-3">
+                <button>Previous Keyframe</button>
+                <button>Next Keyframe</button>
+              </div>
+            </div>
+          </div>
           <Canvas
             className="border border-rose-500 w-full"
             style={{
               height: "500px",
             }}
           >
-            <Camera
+            {/* <AnimatedBox
               data={data}
               time={time}
               isPlaying={isPlaying}
               setTime={setTime}
               setIsPlaying={setIsPlaying}
-            />
-            <ambientLight intensity={0.1} />
-            <directionalLight position={[0, 0, 5]} color="red" />
-            <gridHelper args={[20, 20, "#888888", "#444444"]} />
-          </Canvas>
-          <Canvas
-            className="border border-rose-500 w-full"
-            style={{
-              height: "500px",
-            }}
-          >
-            <AnimatedBox
-              data={data}
-              time={time}
-              isPlaying={isPlaying}
-              setTime={setTime}
-              setIsPlaying={setIsPlaying}
-            />
+            /> */}
             <ambientLight intensity={0.1} />
             <directionalLight position={[0, 0, 5]} color="red" />
             <gridHelper args={[20, 20, "#888888", "#444444"]} />
@@ -66,15 +80,9 @@ function App() {
             <OrbitControls />
           </Canvas>
         </div>
-        <input
-          type="range"
-          min={0}
-          max={duration}
-          step={0.01}
-          value={time}
-          onChange={handleSliderChange}
-          className="w-full"
-        />
+        <div className="p-2">
+          <Timeline time={time} onChange={handleSliderChange} data={data} />
+        </div>
         <button
           onClick={() => {
             if (time === duration) setTime(0);
@@ -83,6 +91,9 @@ function App() {
         >
           {isPlaying ? "Pause" : "Play"}
         </button>
+        <span className="font-mono">
+          {Number(time).toFixed(2)}/{duration}
+        </span>
       </div>
     </>
   );
---
+++ src/Timeline.tsx
@@ -0,0 +1,52 @@
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import { AnimationData } from "./types";
+import { cn } from "./lib/utils";
+
+const Scrubber: React.FC<React.ComponentProps<typeof SliderPrimitive.Root>> = ({
+  ...props
+}) => {
+  return (
+    <SliderPrimitive.Root
+      ref={props.ref}
+      className="relative flex w-full touch-none select-none items-center"
+      step={0.01}
+      {...props}
+    >
+      <SliderPrimitive.Track className="relative h-10 w-full grow overflow-hidden rounded-sm bg-zinc-800">
+        <SliderPrimitive.Range className="absolute h-full bg-primary" />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className="block h-9 w-1 rounded-full cursor-ew-resize border-none bg-rose-700 shadow transition-colors outline-transparent outline-offset-2 outline-2 focus-visible:outline-zinc-600 focus-visible:ring-rose-700" />
+    </SliderPrimitive.Root>
+  );
+};
+
+export const Timeline: React.FC<{
+  data: AnimationData;
+  time: number;
+  onChange: (value: number) => void;
+}> = ({ data, time, onChange }) => {
+  const { duration, keyframes } = data;
+  return (
+    <div className="flex flex-col relative">
+      <Scrubber
+        min={0}
+        max={duration}
+        value={[time]}
+        onValueChange={(value) => onChange(value[0])}
+      />
+      <div className="flex items-center w-full">
+        {keyframes.map((frame, index) => (
+          <button
+            key={index}
+            className={cn(
+              "absolute h-8 w-0.5 grow overflow-hidden rounded-full bg-amber-500 -translate-y-5 cursor-pointer active:scale-95",
+              frame.time / duration === 1 && "-translate-x-0.5",
+            )}
+            style={{ left: `${(frame.time / duration) * 100}%` }}
+            onClick={() => onChange(frame.time)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
